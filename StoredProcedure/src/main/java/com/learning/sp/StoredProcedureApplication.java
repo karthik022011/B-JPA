1a74f6c61cec42543157ac0f9786a47fba1c861a
karthik1220 <karthikgithub1102@gmail.com>
2024-02-27 20:54:06 +0530

    sp added

---
+++ StoredProcedure/src/main/java/com/learning/sp/StoredProcedureApplication.java
@@ -0,0 +1,13 @@
+package com.learning.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoredProcedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoredProcedureApplication.class, args);
+	}
+
+}
