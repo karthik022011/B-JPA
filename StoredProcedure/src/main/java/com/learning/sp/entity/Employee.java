65bff388805f8f29761c27a728caeab5106b53a0
karthik1220 <karthikgithub1102@gmail.com>
2024-02-27 23:44:55 +0530

    code added

---
+++ StoredProcedure/src/main/java/com/learning/sp/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.learning.sp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long empId;
+    private String name;
+
+    public Long getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Long empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
